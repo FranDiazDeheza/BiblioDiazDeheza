8e5db1ee29b563074c247cbc27f2621132cc0c91
Fran Diaz Deheza <84636613+FranDiazDeheza@users.noreply.github.com>
2022-02-19 14:21:03 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import items from './components/itemConteiner';
+import Navbar from './components/navBar';
+import Items from './components/itemConteiner';
+
+function App() {
+  
+  return (
+    <>
+  
+ <Navbar></Navbar>
+ <br></br>
+ <br></br>
+ <br></br>
+ <h1 id="titu">Nuestros Libros</h1>
+ <Items></Items>
+ 
+  
+ </>
+  );
+}
+
+
+export default App;
---
+++ src/components/itemConteiner.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+const Items = () => {
+
+  return ( 
+      <>  
+
+
+<div class="bg-white">
+  <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+    <h2 class="sr-only">Products</h2>
+
+    <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      <a href="#" class="group">
+        <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+          <img src="https://images.cdn2.buscalibre.com/fit-in/360x360/03/05/03056fc1f0f830b81b7f92a74b5ce27e.jpg" alt="Rebelion en la Granja." class="w-full h-full object-center object-cover group-hover:opacity-75"></img>
+        </div>
+        <h3 class="mt-4 text-sm text-gray-700">Rebelion en la Granja - George Orwell </h3>
+        <p class="mt-1 text-lg font-medium text-gray-900">$455</p>
+      </a>
+
+      <a href="#" class="group">
+        <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+          <img src="https://images.cdn3.buscalibre.com/fit-in/360x360/e4/01/e401faaab2124ec3aeccf86467857807.jpg" alt="GranGastby" class="w-full h-full object-center object-cover group-hover:opacity-75"></img>
+        </div>
+        <h3 class="mt-4 text-sm text-gray-700">El Gran Gatsby - F Scott Fitzgerald</h3>
+        <p class="mt-1 text-lg font-medium text-gray-900">$1795</p>
+      </a>
+
+      <a href="#" class="group">
+        <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+          <img src="https://traficantes.net/sites/default/files/styles/large/public/book_covers//9788466347617.gif?itok=UieZhEbx" alt="Manifesto." class="w-full h-full object-center object-cover group-hover:opacity-75"></img>
+        </div>
+        <h3 class="mt-4 text-sm text-gray-700">Manifiesto Comunista - Marx/Engels</h3>
+        <p class="mt-1 text-lg font-medium text-gray-900">$890</p>
+      </a>
+
+      <a href="#" class="group">
+        <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+          <img src="https://images.cdn1.buscalibre.com/fit-in/360x360/65/3c/653c80e8812293a4061faad35a822642.jpg" alt="FntamaCanter" class="w-full h-full object-center object-cover group-hover:opacity-75"></img>
+        </div>
+        <h3 class="mt-4 text-sm text-gray-700">El fantasma de Canterville - Oscar Wilde</h3>
+        <p class="mt-1 text-lg font-medium text-gray-900">$3500</p>
+      </a>
+
+     
+    </div>
+  </div>
+</div>
+
+
+</>
+      
+      
+  )
+
+  
+
+
+}
+
+export default Items
